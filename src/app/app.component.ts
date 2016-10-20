d9b13947e7e54d9bc4a82ffaf4b5f023f612d54d
zwagner <Zachary.Wagner@charter.com>
2016-10-19 23:04:18 -0500

    setup work experience section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.less']
 })
 export class AppComponent {
-  title = 'app works!';
+  slalomProjects = [
+    {
+      role: "Front-End JavaScript Developer",
+      client: "Networking Solutions Company",
+      duration: "4 months",
+      summary: "Client developing a Salesforce portal to allow customers and partners to access information on their products, including downloads, licenses, and assets.",
+      accomplishments: [
+        "Using Visualforce with jQuery and Bootstrap CSS framework on the front-end to deliver a solid user experience",
+        "Solely responsible for UI/UX implementation - managing project requirements and scope, implementing desired features before deadlines",
+        "Provide input on UI/UX design"
+      ]
+    }
+  ];
 }
